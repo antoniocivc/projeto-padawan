e9f553f24583420484f355e9a70de41630cde509
Antonio C Izael <cin_acampos@pagseguro.com>
2022-03-17 11:18:10 -0300

    v5 da pagina

--- pagina-no-bucket/terraform/s3.tf
+++ pagina-no-bucket/terraform/s3.tf
@@ -6,21 +6,11 @@ data "template_file" "s3-public-policy" {
     }
 }
 
-#module "logs" {
-#  source        = "github.com/chgasparoto/terraform-s3-object-notification"
-#  name          = "${local.domain}-logs"
-#  acl           = "log-delivery-write"
-#  force_destroy = !local.has_domain
-#  tags          = local.common_tags
-#}
-
 module "website" {
   source        = "github.com/chgasparoto/terraform-s3-object-notification"
   name          = local.domain
   acl           = "private"
- 
   policy        = data.template_file.s3-public-policy.rendered
-
   force_destroy = !local.has_domain
   tags          = local.common_tags
   versioning = {
@@ -32,20 +22,4 @@ module "website" {
     index_document = "index.html"
     error_document = "index.html"
   }
-
-#  logging = {
-#    target_bucket = module.logs.name
-#    target_prefix = "access/"
-#  }
 }
-
-#module "redirect" {
-#  source        = "github.com/chgasparoto/terraform-s3-object-notification"
-#  name          = "www.${local.domain}"
-#  acl           = "public-read"
-#  force_destroy = !local.has_domain
-#  tags          = local.common_tags
-#  website = {
-#    redirect_all_requests_to = local.has_domain ? var.domain : module.website.website
-#  }
-#}
