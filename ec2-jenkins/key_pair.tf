572bd549ea79b71483666d86e37dbffec782056e
Antonio C Izael <cin_acampos@pagseguro.com>
2022-07-28 11:58:24 -0300

    comentado criação local de chave .pem e add depends on da aws_key_pair na ec2

--- ec2-jenkins/key_pair.tf
+++ ec2-jenkins/key_pair.tf
@@ -12,9 +12,9 @@ resource "aws_key_pair" "kp" {
 }
 
 # #salva localmente o .pem
-# resource "local_file" "ssh_key" {
-#   filename = "./keys/${aws_key_pair.kp.key_name}.pem"
-#   file_permission = "400"
-#   directory_permission = "700"
-#   content = tls_private_key.pk.private_key_pem
-# }
+resource "local_file" "ssh_key" {
+  filename = "./keys/${aws_key_pair.kp.key_name}.pem"
+  file_permission = "400"
+  directory_permission = "700"
+  content = tls_private_key.pk.private_key_pem
+}
